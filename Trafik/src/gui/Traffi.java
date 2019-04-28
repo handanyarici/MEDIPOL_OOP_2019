64ddaa76558290bf6134bda12136df11c38263c7
HANDAN YARICI <TCHYARICI@turkcell.entp.tgc>
2019-04-28 23:20:25 +0300

    11.hafta

---
+++ Trafik/src/gui/Traffi.java
@@ -0,0 +1,16 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Traffi extends Application{
+
+	private Stage primaryStage;
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		this.primaryStage = primaryStage;
+
+	}
+
+}
--- Trafik/src/sorgu/Main.java
+++ Trafik/src/sorgu/Main.java
@@ -3,6 +3,7 @@ package sorgu;
 import java.io.File;
 import java.util.Scanner;
 
+import rapor.Ceza;
 import rapor.Suclu;
 import util.FileUtil;
 
@@ -14,21 +15,45 @@ public class Main {
 		System.out.println("tc kimlik no giriniz");
 		String tckimlik = s.nextLine();
 		String vatandaslar = FileUtil.readFile(new File("vatandas.txt"));
-		System.out.println(vatandaslar);
 		String[] vList = vatandaslar.split("\n");
+		String cezaDosyasi = FileUtil.readFile(new File("cezalar.txt"));
+		String[] vCezalar = cezaDosyasi.split("\n");
+		Ceza[] cezalar = new Ceza[vCezalar.length];
+		for(int i = 0; i< vCezalar.length;i++){
+			String[] alanlar = vCezalar[i].split(";");
+			Ceza ceza = new Ceza();
+			ceza.setId(Long.parseLong(alanlar[0]));
+			ceza.setCezaMiktari(Double.parseDouble(alanlar[2]));
+			ceza.setTanim(alanlar[1]);
+			cezalar[i]=ceza;
+		}
+		boolean flag = false;
 		for(String satir:vList){
 			String[] degerler = satir.split(";");
-			if(degerler.length >= 2){
-				//bu bir suclu
-				Vatandas suclu = new Suclu(degerler[0]);
-			}else{
-				//sucsuz vatandas
-				System.out.println("Borcunuz bulunmamaktadir");
+			if(degerler[0].equals(tckimlik)){
+				flag=true;
+				if(degerler.length >= 2){
+					//bu bir suclu
+					Vatandas suclu = new Suclu(degerler[0]);
+					double toplamborc=0;
+					String[] kisiCezalari = degerler[1].split(",");
+					for(String cezaId:kisiCezalari){
+						for(Ceza ceza:cezalar){
+							if(ceza.getId() == Long.parseLong(cezaId)){
+								toplamborc += ceza.getCezaMiktari();
+							}
+						}
+					}
+					System.out.println("Toplam borcunuz:" + toplamborc);
+				}else{
+					//sucsuz vatandas
+					System.out.println("Borcunuz bulunmamaktadir");
+				}
 			}
 		}
-		String cezalar = FileUtil.readFile(new File("cezalar.txt"));
-		System.out.println("------------------------------------------");
-		System.out.println(cezalar);
+		if(!flag){
+			System.out.println("Aradiginiz kisi bulunamadi");
+		}
 	}
 
 }
